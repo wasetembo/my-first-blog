0ad0a60b007238e0d7f53413ab1789e6afdd70d8
wasetembo <wasetembo@gmail.com>
2024-12-08 19:43:31 -0600

    added logins

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from .models import Post, Comment
+
 
 # Register your models here.
 from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+admin.site.register(Comment)
